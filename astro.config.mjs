c684d04a50ea08a9fc419293642fe0df7a7912a1
DrMint <29893320+DrMint@users.noreply.github.com>
2025-02-17 13:51:41 +0100

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import rehypeUnwrapImages from "rehype-unwrap-images";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({ optimize: true })],
+  integrations: [mdx({ optimize: true }), sitemap()],
   trailingSlash: "never",
   markdown: {
     rehypePlugins: [rehypeUnwrapImages],
@@ -16,4 +17,5 @@ export default defineConfig({
   server: {
     host: true,
   },
+  site: "http://localhost:4321",
 });
