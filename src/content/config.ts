fba6ca0c3649d487ace41cfb7b25949e2e6e85ad
DrMint <29893320+DrMint@users.noreply.github.com>
2025-02-20 20:31:21 +0100

    Add alt on post cover images

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const postsCollection = defineCollection({
     z.object({
       title: z.string(),
       cover: image(),
+      coverAlt: z.string(),
       summary: z.string().optional(),
       date: z.coerce.date(),
       tags: z.array(z.string()).optional(),
