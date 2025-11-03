74555b38ddbddcce2145bce24c7aa98cb052d471
DrMint <29893320+DrMint@users.noreply.github.com>
2025-11-03 18:43:36 +0100

    Added pm2 runner script

---
+++ run_r-entries.sh
@@ -0,0 +1,10 @@
+export SITE_URL="https://r-entries.com"
+export BASE_PATH="/"
+export TRAILING_SLASH="never"
+export PORT="45399"
+
+git pull
+rm -r dist
+npm ci
+npm run build
+npm run preview -- --allowed-hosts=r-entries.com
